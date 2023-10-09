779e54df2c548307ae0ed98e1c3828acb5e829a1
MauricioCuervo2023 <127361506+MauricioCuervo2023@users.noreply.github.com>
2023-10-08 21:27:09 -0500

    cambio titulo
    Club Deportivo Morichal

--- principal/urls.py
+++ principal/urls.py
@@ -4,5 +4,5 @@ from principal.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio, name="Club Deportivo Morichal"),
+    path('', inicio, name="iniciio"),
 ]
