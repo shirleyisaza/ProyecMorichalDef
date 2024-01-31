6de1c352d4ea6ab1edfcd8b373678772fbc91dee
shirleyisaza <127167701+shirleyisaza@users.noreply.github.com>
2024-01-30 23:11:41 -0500

    por el momento funciona

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from usuarios.forms import UsuarioForm
+from usuarios.models import Usuario
 
 # Create your views here.
+
+
+def usuarios(request):
+    usuarios= Usuario.objects.all()
+    
+    cotext={
+    
+  "usuarios":usuarios 
+       
+    }
+    return render(request,'usuarios/usuarios.html',context)
+
+def usuarios_crear(request):
+    
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("El núero usuario se guardo correctamente")
+            
+            return redirect('usuarios')
+        else:
+            print("El núero usuario no guardo")
+    else:
+               
+        form= UsuarioForm()
+                
+    context={
+        "form":form
+    }
+    return render(request, 'usuarios/usuarios-crear.html',context)
