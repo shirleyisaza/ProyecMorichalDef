505e80578930eba7ad0621e58c53ee9cb633136d
MauricioCuervo2023 <127361506+MauricioCuervo2023@users.noreply.github.com>
2024-03-24 21:35:21 -0500

    cambios
    ok

--- deportista/models.py
+++ deportista/models.py
@@ -4,8 +4,8 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Deportista(models.Model):
     genero = models.TextField(max_length=45, verbose_name="Genero")
-    lugarNacimiento = models.TextField(max_length=45, verbose_name="Lugar de Nacimiento")
-    fechaNacimiento = models.DateField(verbose_name="Fecha de Nacimiento", help_text="MM/DD/AAAA")
+    lugar_nacimiento = models.TextField(max_length=45, verbose_name="Lugar de Nacimiento")
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de Nacimiento", help_text="MM/DD/AAAA")
     edad = models.IntegerField(verbose_name="Edad")
     peso = models.IntegerField(verbose_name="Peso en Kg")
     estatura = models.IntegerField(verbose_name="Estatura")
